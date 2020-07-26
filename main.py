4d5d0667759ca878b8bd730fcec3e2fb06bb64eb
Deepanshu Gautam <ishugautam29@gmail.com>
2020-07-26 10:30:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#groceries = {'bananas': 5, 'apple': 9}
+
+#print(groceries.get('hello'))
+
+#contacts = {
+  #'Papa': {'Phone': '9838608732', 'email': 'babu.gautam4.com'}, 
+  #'Mummy': {'Phone': '6306865407', 'email': 'suman1972.com'}
+#}
+
+#print(contacts.get('Papa'))
+
+
+
+
+Sentence = 'I like the name Inasnity coz the name Insanity boosts up my digital work confidence'
+
+
+word_counts = {
+  'like': 1,
+  'the': 2,
+  'name': 2,
+  'Insanity': 2
+}
+
+#dict.items()
+#print(list(word_counts.items()))
+
+#dict.keys()
+#print(list(word_counts.keys()))
+
+#dict.values()
+print(list(word_counts.values()))
